2eacfb11bfa07656fd3dda5ea9ffe3ee2b8a1203
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-06 23:45:28 -0500

    Update book.php

--- book.php
+++ book.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/x-icon" href="butterflykissesluxurypicnic.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link rel="stylesheet" type="text/css" href="script.js">
 </head>
@@ -75,7 +76,14 @@
 <div id="message" style="display: none;">Thanks for Booking Us. We are Excited to Be At Your Event. See you Soon!</div>
     </div>
     </main>
-    
+
+    <div class="socials-section">
+    <a href="https://www.instagram.com/butterflykisses_luxurypicnic/" class="social-link" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/profile.php?id=100083296629256" class="social-link" target="_blank"><i class="fab fa-facebook"></i></a>
+    <a href="https://www.tiktok.com/belindahouse0" class="social-link" target="_blank"><i class="fab fa-tiktok"></i></a>
+    <a href="https://www.instagram.com/house_belinda/" class="social-link" target="_blank"><i class="fab fa-instagram"></i></a>
+    </div>
+            
     <footer>
         <div class="container">
         &copy; 2024 Butterfly Kisses Luxury Picnic. All rights reserved. | <a href="mailto:belindahouse40@gmail.com">belindahouse40@gmail.com</a><br>
