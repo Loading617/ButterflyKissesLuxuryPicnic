a8d530961fbdbae56cfffb77e54a6472a06dfe82
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-31 13:02:22 -0500

    Update belindahouse.php

--- belindahouse.php
+++ belindahouse.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head <?php echo""; ?>>
     <title>Butterfly Kisses Luxury Picnic ▦ Belinda House</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body>
+<body <?php echo""; ?>>
     
 <header>
   <div class="container">
