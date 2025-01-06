2e05070644a6e194480159e2f02dfe41932c1c8f
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-06 17:58:57 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -68,7 +68,7 @@
                     
             <footer>
                 <div class="container">
-                &copy; 2024 Butterfly Kisses Luxury Picnic. All rights reserved. | Address Coming Soon! | Columbia, SC Event Planner
+                &copy; 2022-2025 Butterfly Kisses Luxury Picnic. All rights reserved. | Address Coming Soon! | Columbia, SC Event Planner
                 </div>
             </footer>
         </div>
