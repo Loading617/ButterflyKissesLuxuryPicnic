2f413b014f9ddfd976b86be793b47f60f0e813de
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-08-26 00:05:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -82,10 +82,10 @@
     </main>
 
     <div class="social-section">
-    <a href="https://www.instagram.com/butterflykisses_luxurypicnic/" class="social-link" target="_blank"><i class="fab fa-instagram"></i></a>
-    <a href="https://www.facebook.com/profile.php?id=100083296629256" class="social-link" target="_blank"><i class="fab fa-facebook"></i></a>
-    <a href="https://www.tiktok.com/belindahouse0" class="social-link" target="_blank"><i class="fab fa-tiktok"></i></a>
-    <a href="https://www.instagram.com/house_belinda/" class="social-link" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.instagram.com/butterflykisses_luxurypicnic/" class="social-media-link" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/profile.php?id=100083296629256" class="social-media-link" target="_blank"><i class="fab fa-facebook"></i></a>
+    <a href="https://www.tiktok.com/belindahouse0" class="social-media-link" target="_blank"><i class="fab fa-tiktok"></i></a>
+    <a href="https://www.instagram.com/house_belinda/" class="social-media-link" target="_blank"><i class="fab fa-instagram"></i></a>
     </div>
 
     <footer>
@@ -98,6 +98,3 @@
     </footer>
 </body>
 </html>
-
-
-
