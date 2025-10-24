d45a80675124b0ff059cc90b228cf4c7fc10300a
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-10-23 21:40:19 -0400

    Fix HTML structure and add dynamic year in footer

--- picnic.php
+++ picnic.php
@@ -59,7 +59,7 @@
             <footer>
                 <div class="container">
                 <p>
-                &copy; 2022-2025 Butterfly Kisses Luxury Picnic. All rights reserved. | <a href="mailto:belindahouse40@gmail.com">belindahouse40@gmail.com</a><br>
+                &copy; <span id="year"></span> Butterfly Kisses Luxury Picnic. All rights reserved. | <a href="mailto:belindahouse40@gmail.com">belindahouse40@gmail.com</a><br>
                 <a href="tel:+8039154747">(803) 915-4747</a> | Address Coming Soon! | Columbia, SC Event Planner
                 </p>
                 </div>
@@ -67,4 +67,5 @@
         </div>
     </body>
     </html>
+
 
